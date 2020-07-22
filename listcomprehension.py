03abb67ecd81abb3968c43d35cb80f4c6e3f7f67
Cami <garciacamilalorena@gmail.com>
2020-07-22 19:02:45 -0300

    added example of the same problem solved with filter function

--- listcomprehension.py
+++ listcomprehension.py
@@ -3,3 +3,7 @@
 pares = [x for x in range(0, 11) if x % 2 == 0]
 print(len(pares))
 #imprime la cantidad de pares que hay de 0 a 10
+
+#otra alternativa es utilizar fuciones como map y filter
+pares = list(filter(lambda par: par % 2 == 0, range(0, 11)))
+print(len(pares))
